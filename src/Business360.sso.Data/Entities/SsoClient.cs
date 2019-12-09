f98dd7c9b18862828db41aeb3f8c6dd25ddc541b
Olabode Aguda <olabode.aguda@venturegardengroup.com>
2019-12-10 00:09:47 +0100

    client update

---
+++ src/Business360.sso.Data/Entities/SsoClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business360.sso.Data.Entities
+{
+    public class SsoClient
+    {
+        public long Id { get; set; }
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientUri { get; set; }
+        public string AllowedGrantTypes { get; set; }
+        public string RedirectUris { get; set; }
+        public string PostLogoutRedirectUris { get; set; }
+        public string AllowedCorsOrigins { get; set; }
+
+        public ICollection<SsoScope> SsoScopes { get; set; }
+    }
+}
